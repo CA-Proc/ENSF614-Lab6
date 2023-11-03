c5ee6b7a22f7e6518052874b0dc3e159c394ba13
Chris Proc <chris.a.proc@gmail.com>
2023-11-02 19:06:33 -0600

    Templating done

--- iterator.cpp
+++ iterator.cpp
@@ -7,47 +7,48 @@
 
 using namespace std;
 
+template <class T>
 class Vector {
 public: 
 
   class VectIter{
     friend class Vector;
   private:
-    Vector *v; // points to a vector object of type T
+    Vector<T> *v; // points to a vector object of type T
     int index;    // represents the subscript number of the vector's
                   // array.
   public:
-    VectIter(Vector& x);
+    VectIter(Vector<T>& x);
  
-    int operator++();
+    T operator++();
     //PROMISES: increments the iterator's indes and return the 
     //          value of the element at the index position. If
     //          index exceeds the size of the array it will 
     //          be set to zero. Which means it will be circulated
     //          back to the first element of the vector.
 
-    int  operator++(int);
+    T  operator++(int);
     // PRIMISES: returns the value of the element at the index
     //           position, then increments the index. If
     //           index exceeds the size of the array it will 
     //           be set to zero. Which means it will be circulated
     //           back to the first element of the vector.
 
-    int  operator--();
+    T  operator--();
     // PROMISES: decrements the iterator index, and return the
     //           the value of the element at the index. If
     //           index is less than zero it will be set to the 
     //           last element in the aray. Which means it will be
     //           circulated to the last element of the vector.
 
-    int  operator--(int);
+    T  operator--(int);
     // PROMISES: returns the value of the element at the index
     //           position, then decrements the index. If
     //           index is less than zero it will be set to the 
     //           last element in the aray. Which means it will be
     //           circulated to the last element of the vector.
 
-    int operator *();
+    T operator *();
     // PROMISES: returns the value of the element at the current 
     //           index position.
 
@@ -61,7 +62,7 @@ public:
   Vector(int sz); 
   ~Vector();
 
-  int & operator[](int i);
+  T & operator[](int i);
   // PROMISES: returns existing value in the ith element of 
   //           array or sets a new value to  the ith element in
   //           array. 
@@ -72,12 +73,12 @@ public:
 private:
   int *array;               // points to the first element of an array of T
   int size;               // size of array
-  void swap(int&, int &); // swaps the values of two elements in array
+  void swap(T&, T&); // swaps the values of two elements in array
 public:
 };
 
-
-void Vector::ascending_sort()
+template <class T>
+void Vector<T>::ascending_sort()
 {
 	for(int i=0; i< size-1; i++)
 		for(int j=i+1; j < size; j++)
@@ -85,19 +86,22 @@ void Vector::ascending_sort()
 				swap(array[i], array[j]);
 }
 
-void Vector::swap(int& a, int& b)
+template <class T>
+void Vector<T>::swap(T& a, T& b)
 {
-	int tmp = a;
+	T tmp = a;
 	a = b;
 	b = tmp;
 }
 
-int Vector::VectIter::operator *()
+template <class T>
+T Vector<T>::VectIter::operator *()
 {
   return v -> array[index];
 }
 
-int Vector::VectIter::operator ++(){
+template <class T>
+T Vector<T>::VectIter::operator ++(){
   if (index == (v -> size)-1){
     index = 0;
   } else {
@@ -106,7 +110,8 @@ int Vector::VectIter::operator ++(){
   return v -> array[index];
 }
 
-int Vector::VectIter::operator ++(int){
+template <class T>
+T Vector<T>::VectIter::operator ++(int){
   int val = v -> array[index];
 
   if (index == (v -> size)-1){
@@ -118,7 +123,8 @@ int Vector::VectIter::operator ++(int){
   return val;
 }
 
-int Vector::VectIter::operator --(){
+template <class T>
+T Vector<T>::VectIter::operator --(){
   if (index == 0){
     index = (v -> size)-1;
   } else {
@@ -127,7 +133,8 @@ int Vector::VectIter::operator --(){
   return v -> array[index];
 }
 
-int Vector::VectIter::operator --(int){
+template <class T>
+T Vector<T>::VectIter::operator --(int){
   int val = v -> array[index];
 
   if (index == 0){
@@ -139,31 +146,31 @@ int Vector::VectIter::operator --(int){
   return val;
 }
 
-Vector::VectIter::VectIter(Vector& x)
+template <class T>
+Vector<T>::VectIter::VectIter(Vector<T>& x)
 {
   v = &x;
   index = 0;
 }
 
-
-
-Vector::Vector(int sz)
+template <class T>
+Vector<T>::Vector(int sz)
 {
   size=sz;
-  array = new int [sz];
+  array = new T [sz];
   assert (array != NULL);
 }
 
-
-Vector::~Vector()
+template <class T>
+Vector<T>::~Vector()
 {
   delete [] array;
   array = NULL;
 }
 
 
-
-int & Vector ::operator [] (int i)
+template <class T>
+T & Vector<T>::operator [] (int i)
 {
   return array[i];
 }
@@ -172,12 +179,12 @@ int & Vector ::operator [] (int i)
 int main()
 {
 
- Vector x(3);
+ Vector<int> x(3);
  x[0] = 999;
  x[1] = -77;
  x[2] = 88;
 
- Vector::VectIter iter(x);
+ Vector<int>::VectIter iter(x);
 
  cout << "\n\nThe first element of vector x contains: " << *iter << endl; 
 
@@ -190,7 +197,7 @@ int main()
  // the code between the  #if 0 and #endif is ignored by
  // compiler. If you change it to #if 1, it will be compiled
  
-#if 0
+#if 1
 	cout << "\nTesting an <int> Vector: " << endl;
 	
 	cout << "\n\nTesting sort";
